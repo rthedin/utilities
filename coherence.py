298de06252dfe89cb6e7201c9a948fe25ae85420
Regis Thedin <registhedin@gmail.com>
2023-03-06 16:32:11 -0700

    Add extra coherence functions

--- coherence.py
+++ coherence.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import xarray as xr
+import os
 import matplotlib.pyplot as plt
 
 from mmctools.helper_functions import calc_spectra
@@ -114,14 +115,32 @@ def calc_coherence_2signals(s1,s2, strname=None, interval='120min', window_lengt
     qucoh = xr.merge([ ((psd['u1u2']/((psd['u1']*psd['u2'])**0.5)).imag).to_dataset(name=f'qucoh_{strname}sep_u1u2'),
                        ((psd['v1v2']/((psd['v1']*psd['v2'])**0.5)).imag).to_dataset(name=f'qucoh_{strname}sep_v1v2'),
                        ((psd['w1w2']/((psd['w1']*psd['w2'])**0.5)).imag).to_dataset(name=f'qucoh_{strname}sep_w1w2') ])
-    
+   
+    qucoh_ =xr.merge([ ( psd['u1u2'].imag/(psd['u1']*psd['u2'])**0.5 ).to_dataset(name=f'qucohnew_{strname}sep_u1u2'),
+                       ( psd['v1v2'].imag/(psd['v1']*psd['v2'])**0.5 ).to_dataset(name=f'qucohnew_{strname}sep_v1v2'),
+                       ( psd['w1w2'].imag/(psd['w1']*psd['w2'])**0.5 ).to_dataset(name=f'qucohnew_{strname}sep_w1w2') ])
 
-    return xr.merge([mscoh,cocoh,qucoh]), psd
+    xpsd  = xr.merge([ (psd['u1u2']).to_dataset(name=f'xpsd_{strname}sep_u1u2'),
+                       (psd['v1v2']).to_dataset(name=f'xpsd_{strname}sep_v1v2'),
+                       (psd['w1w2']).to_dataset(name=f'xpsd_{strname}sep_w1w2') ])
+
+    psd_ =xr.merge([ (psd['u1']).to_dataset(name=f'psd_{strname}sep_u1'),
+                    (psd['v1']).to_dataset(name=f'psd_{strname}sep_v1'),
+                    (psd['w1']).to_dataset(name=f'psd_{strname}sep_w1'),
+                    (psd['v2']).to_dataset(name=f'psd_{strname}sep_u2'),
+                    (psd['v2']).to_dataset(name=f'psd_{strname}sep_v2'),
+                    (psd['w2']).to_dataset(name=f'psd_{strname}sep_w2') ])
+
+    radius =xr.merge([ ( (psd['u1u2'].real**2+psd['u1u2'].imag**2)**0.5/(psd['u1']*psd['u2'])**0.5 ).to_dataset(name=f'radius_{strname}sep_u1u2'),
+                       ( (psd['v1v2'].real**2+psd['v1v2'].imag**2)**0.5/(psd['v1']*psd['v2'])**0.5 ).to_dataset(name=f'radius_{strname}sep_v1v2'),
+                       ( (psd['w1w2'].real**2+psd['w1w2'].imag**2)**0.5/(psd['w1']*psd['w2'])**0.5 ).to_dataset(name=f'radius_{strname}sep_w1w2') ])
+
+    return xr.merge([mscoh,cocoh,qucoh,qucoh_,xpsd, psd_, radius]), psd
 
 
 
 
-def plotCoherence(coh_sep, sep_list, meandim, umean=None, **kwargs):
+def plotCoherence(coh_sep, sep_list, meandim, umean=None, xaxis='freq', **kwargs):
     '''
     Plot mscoh, co-coherence, and quad-coherence from coh_sep dataset
     
@@ -134,6 +153,8 @@ def plotCoherence(coh_sep, sep_list, meandim, umean=None, **kwargs):
         Direction to compute the mean. Should be 'x', 'y', or 'z'
     umean: float
         Mean velocity used to compute IEC coherence. IEC curves are skipped is umean is not specified
+    xaxis: str ('freq', or 'redfreq'; default 'freq')
+        Quantity to use on the xaxis
     a, b: float
         Values for a and B for Davenport's model
     
@@ -141,9 +162,12 @@ def plotCoherence(coh_sep, sep_list, meandim, umean=None, **kwargs):
 
     import matplotlib.colors as mcolors
     
+    # Check what xaxis was requested
+    if xaxis not in {'freq','redfreq'}:
+        raise ValueError (f'The argument xaxis can only take `freq` or `redfreq`.')
+
     # Get auxiliary arrays
     colors = list(mcolors.TABLEAU_COLORS)
-    f = coh_sep.frequency
     sepcoord = [coord for coord in list(coh_sep.coords.keys()) if coord.startswith('sep')][0]
     if  sepcoord[-1] == 'x':
         cohsepstr = 'lon'
@@ -154,7 +178,6 @@ def plotCoherence(coh_sep, sep_list, meandim, umean=None, **kwargs):
     else:
         raise ValueError (f'A coordinate named sep_x, sep_y, or sep_z must exist. Stopping')
 
-        
     # Let's check if all the separations exist in the dataset before plotting
     if not set(sep_list).issubset(set(coh_sep[sepcoord].values)):
         raise ValueError (f'Not all separation distances requested to plot are available. Stopping')
@@ -173,34 +196,39 @@ def plotCoherence(coh_sep, sep_list, meandim, umean=None, **kwargs):
     fig, axs = plt.subplots(3,3,figsize=(16,9), sharey=True, sharex=True)
 
     for c, sep in enumerate(sep_list):
+
+        # get the frequency axis
+        if xaxis == 'freq': f = coh_sep.frequency
+        else:               f = coh_sep.frequency * sep / umean
+
         # mscoh uu
-        axs[0,0].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'mscoh_{cohsepstr}sep_u1u2'], c=colors[c%len(colors)], label=f'{sepcoord} = {sep}')
+        axs[0,0].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'mscoh_{cohsepstr}sep_u1u2'], c=colors[c%len(colors)], label=f'{sep}')
         if umean is not None:
             #axs[0,0].plot(f, IECCoherence(f,umean,delta=sep,component='u',mode='sameValues'), c=colors[c%len(colors)], ls='--', alpha=0.7)
             axs[0,0].plot(f, davenportExpCoh(f,u=umean,delta=sep,Lc='defaultu',**kwargs), c=colors[c%len(colors)], ls='--', alpha=0.7)
         # co-coh uu
-        axs[0,1].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'cocoh_{cohsepstr}sep_u1u2'], c=colors[c%len(colors)], label=f'{sepcoord} = {sep}')
+        axs[0,1].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'cocoh_{cohsepstr}sep_u1u2'], c=colors[c%len(colors)], label=f'{sep}')
         if umean is not None:
             #axs[0,1].plot(f, IECCoherence(f,umean,delta=sep,component='u',mode='sameValues'), c=colors[c%len(colors)], ls='--', alpha=0.7)
             axs[0,1].plot(f, davenportExpCoh(f,u=umean,delta=sep,Lc='defaultu',**kwargs), c=colors[c%len(colors)], ls='--', alpha=0.7)
         # quad-coh uu
-        axs[0,2].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'qucoh_{cohsepstr}sep_u1u2'], c=colors[c%len(colors)], label=f'{sepcoord} = {sep}')
+        axs[0,2].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'qucoh_{cohsepstr}sep_u1u2'], c=colors[c%len(colors)], label=f'{sep}')
 
         # ---------------------
         # mscoh vv
-        axs[1,0].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'mscoh_{cohsepstr}sep_v1v2'], c=colors[c%len(colors)], label=f'{sepcoord} = {sep}')
+        axs[1,0].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'mscoh_{cohsepstr}sep_v1v2'], c=colors[c%len(colors)], label=f'{sep}')
         # co-coh vv
-        axs[1,1].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'cocoh_{cohsepstr}sep_v1v2'], c=colors[c%len(colors)], label=f'{sepcoord} = {sep}')
+        axs[1,1].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'cocoh_{cohsepstr}sep_v1v2'], c=colors[c%len(colors)], label=f'{sep}')
         # quad-coh vv
-        axs[1,2].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'qucoh_{cohsepstr}sep_v1v2'], c=colors[c%len(colors)], label=f'{sepcoord} = {sep}')
+        axs[1,2].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'qucoh_{cohsepstr}sep_v1v2'], c=colors[c%len(colors)], label=f'{sep}')
 
         # ---------------------
         # mscoh vv
-        axs[2,0].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'mscoh_{cohsepstr}sep_w1w2'], c=colors[c%len(colors)], label=f'{sepcoord} = {sep}')
+        axs[2,0].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'mscoh_{cohsepstr}sep_w1w2'], c=colors[c%len(colors)], label=f'{sep}')
         # co-coh vv
-        axs[2,1].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'cocoh_{cohsepstr}sep_w1w2'], c=colors[c%len(colors)], label=f'{sepcoord} = {sep}')
+        axs[2,1].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'cocoh_{cohsepstr}sep_w1w2'], c=colors[c%len(colors)], label=f'{sep}')
         # quad-coh vv
-        axs[2,2].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'qucoh_{cohsepstr}sep_w1w2'], c=colors[c%len(colors)], label=f'{sepcoord} = {sep}')
+        axs[2,2].plot(f, coh_sep.mean(dim=meandim).sel({sepcoord:sep})[f'qucoh_{cohsepstr}sep_w1w2'], c=colors[c%len(colors)], label=f'{sep}')
 
 
     axs[0,0].set_title(f'mscoh $\gamma^2_{{uu, {cohsepstr}}}$', fontsize=13)
@@ -216,14 +244,16 @@ def plotCoherence(coh_sep, sep_list, meandim, umean=None, **kwargs):
     axs[2,2].set_title(f'quad-coh $\\rho_{{ww, {cohsepstr}}}$', fontsize=13)
 
 
+    if xaxis == 'freq': xlabel = 'freq [Hz]'
+    else:               xlabel = 'reduced freq (f*d/u) [-]'
     for ax in axs.flatten():
         if ylim is not None:
             ax.set_ylim(ylim)
         if xlim is not None:
             ax.set_xlim(xlim)
         ax.grid()
-    for ax in axs[:,-1]:  ax.legend(fontsize=13)
-    for ax in axs[-1,:]:  ax.set_xlabel('freq [Hz]', fontsize=14)
+    axs[0,-1].legend(title=f'{sepcoord} (m)', fontsize=12, title_fontsize=13, loc='upper left', bbox_to_anchor=(1,1))
+    for ax in axs[-1,:]:  ax.set_xlabel(xlabel, fontsize=14)
     for ax in axs[:,0]:   ax.set_ylabel('coherence', fontsize=14)
     fig.tight_layout()
 
@@ -308,5 +338,208 @@ def davenportExpCoh(f,u,delta,Lc='defaultu',a=None,b=None,B=None, **kwargs):
     return np.exp( -a*np.sqrt( (f*delta/u)**2 + (B*delta)**2) )
 
     
+
+
+def calcVertCoh(dsx, sep_z_list, zref, interval, window_length, window, ydist, outputPath=None):
+    '''
+    Calculates vertical coherence
+    
+    Inputs
+    ======
+    dsx: xarray dataset
+        Array containing perturbation valocity (up,vp,wp) at coordinates ([x,]y,z,datetime)
+    sep_z_list: list of scalars
+        Vertical separation values to compute coherence
+    zref: scalar
+        Reference z location (typically 150, 151.5, etc)
+    interval: string
+        Time interval of the whole dataset (typically '180min')
+    window_length: string
+        Window length for windowing approach (typically '5min' or '2min')
+    window: string
+        Windowing algorithm to use (typically 'hanning')
+    ydist: scalar
+        Lateral distance at which another pair of points will be collected (typically grid resolution)
+    outputPath: string
+        Path where a zarr file will be saved to
+        
+    Example call
+    ============
+    # Location of reference point
+    zref = 151
+    # Total length of the time series
+    interval='180min'
+    # Window parameters
+    window_length = '2min'
+    window = 'hanning'
+    # At every ydist laterally (in y), get another pair of points
+    ydist = 2.5
+    # Vertical separation values of interest
+    sep_z_list = [5, 10, 15, 25, 40, 80, 120]
+    # At every ydist laterally (in y), get another pair of points
+    ydist = 2.5
+    
+    coh_zsep = calcVertCoh(ds_cohu, sep_z_list, zref, interval, window_length, window, ydist, outpath)
+    
+    '''
+    
+    # ------------------------------ SETTINGS FOR VERTICAL SEPARATION COHERENCE
+    # --------------------------------------------------- USING X-NORMAL PLANES
+    # At every ydist laterally (in y), let's get another pair of points
+    y_loc_list = np.arange(dsx.y.min(), dsx.y.max()-ydist, ydist)
+
+    # Planes to loop on (if exists; will not exist in turbsim data)
+    if 'x' in list(dsx.coords.keys()):
+        xplanes = dsx.x.values
+    else:
+        xplanes = [None]
+    # -------------------------------------------------------------------------
+
+
+    if os.path.isdir(os.path.join(str(outputPath),f'coh_zsep_xnormal_{window_length}.zarr')):
+        coh_zsep_xnormal = xr.open_zarr(os.path.join(outputPath,f'coh_zsep_xnormal_{window_length}.zarr'))
+        return coh_zsep_xnormal
+ 
+    # On each x-normal plane, loop through the separation list and the pairs of points, accumulating the results into a single dataset
+    coh_zsep_xnormal = []
+    for xplane in xplanes:
+        coh_zsep = []
+        for sep_z in sep_z_list:
+            if xplane is None:
+                dsx_currentxplane = dsx
+            else:
+                dsx_currentxplane = dsx.sel(x=xplane)
+                
+            print(f'Computing the vertical coherence on plane x={xplane} between (y={y_loc_list[0]}--{y_loc_list[-1]},z={zref})',
+                  f'and (y={y_loc_list[0]}--{y_loc_list[-1]},z={zref+sep_z}), for a vertical separation of {sep_z} m')
+            
+            coh_y=[]
+            for y in y_loc_list:
+                # Get the two signals
+                s1={'ds':dsx_currentxplane, 'z':zref, 'y':y}
+                s2={**s1, 'z':zref+sep_z}
+
+                mycoh, mypsd = calc_coherence_2signals(s1,s2, strname='vert', interval=interval, window_length=window_length, window=window, normal='xNormal')
+                mycoh = mycoh.expand_dims('y').assign_coords({'y':[y]})
+                coh_y.append(mycoh)
+
+            coh_y = xr.concat(coh_y, dim='y')
+            coh_zsep.append(coh_y.expand_dims('sep_z').assign_coords({'sep_z':[sep_z]}))
+        coh_zsep = xr.concat(coh_zsep, dim='sep_z')
+        coh_zsep_xnormal.append(coh_zsep.expand_dims('xplane').assign_coords({'xplane':[xplane]}))
+    coh_zsep_xnormal = xr.concat(coh_zsep_xnormal, dim='xplane')
+
+    # Get rid of the single xplane level for single xplanes (TurbSim data, for instance)
+    coh_zsep_xnormal = coh_zsep_xnormal.squeeze(drop=True)
+    
+    if outputPath is not None:
+        coh_zsep_xnormal.to_zarr(os.path.join(outputPath,f'coh_zsep_xnormal_{window_length}.zarr'))
+    
+    return coh_zsep_xnormal
+
+
+
+
+def calcLatCoh(dsx, sep_y_list, yref, zref, interval, window_length, window, ydist, outputPath=None):
+    '''
+    Calculates lateral coherence
+    
+    Inputs
+    ======
+    dsx: xarray dataset
+        Array containing perturbation valocity (up,vp,wp) at coordinates ([x,]y,z,datetime)
+    sep_y_list: list of scalars
+        Lateral separation values to compute coherence
+    yref: scalar
+        Reference y location (typically 0, even ymin=-100)
+    zref: scalar
+        Reference z location (typically 150, 151.5, etc)
+    interval: string
+        Time interval of the whole dataset (typically '180min')
+    window_length: string
+        Window length for windowing approach (typically '5min' or '2min')
+    window: string
+        Windowing algorithm to use (typically 'hanning')
+    ydist: scalar
+        Lateral distance at which another pair of points will be collected (typically grid resolution)
+    outputPath: string
+        Path where a zarr file will be saved to
+        
+    Example call
+    ============
+    # Location of reference point
+    yref = 0
+    zref = 151
+    # Total length of the time series
+    interval='180min'
+    # Window parameters
+    window_length = '2min'
+    window = 'hanning'
+    # At every ydist laterally (in y), get another pair of points
+    ydist = 2.5
+    # Lateral separation values of interest
+    sep_y_list = [5, 10, 15, 25, 40, 80, 120]
+    # At every ydist laterally (in y), get another pair of points
+    ydist = 2.5
+    
+    coh_ysep = calcLatCoh(ds_cohu, sep_y_list, yref, zref, interval, window_length, window, ydist, outpath)
+    
+    '''
+    
+    # ------------------------------- SETTINGS FOR LATERAL SEPARATION COHERENCE
+    # --------------------------------------------------- USING X-NORMAL PLANES
+    # At every ydist laterally (in y), get another pair of points
+    y_loc_list = np.arange(dsx.y.min(), dsx.y.max()-max(sep_y_list), ydist)
+
+    # Planes to loop on (if exists; will not exist in turbsim data)
+    if 'x' in list(dsx.coords.keys()):
+        xplanes = dsx.x.values
+    else:
+        xplanes = [None]
+    # -------------------------------------------------------------------------
+
+
+    if os.path.isdir(os.path.join(str(outputPath),f'coh_ysep_xnormal_{window_length}.zarr')):
+        coh_ysep_xnormal = xr.open_zarr(os.path.join(outputPath,f'coh_ysep_xnormal_{window_length}.zarr'))
+        return coh_ysep_xnormal
+    
+    # On each x-normal plane, loop through the separation list and the pairs of points, accumulating the results into a single dataset
+    coh_ysep_xnormal = []
+    for xplane in xplanes:
+        coh_ysep = []
+        for sep_y in sep_y_list:
+            if xplane is None:
+                dsx_currentxplane = dsx
+            else:
+                dsx_currentxplane = dsx.sel(x=xplane)
+                
+            print(f'Computing the lateral coherence on plane x={xplane} between (y={yref+y_loc_list[0]}--{yref+y_loc_list[-1]},z={zref})',
+                  f'and (y={yref+y_loc_list[0]+sep_y}--{yref+y_loc_list[-1]+sep_y},z={zref}), for a lateral separation of {sep_y} m')
+            
+            coh_y=[]
+            for y in y_loc_list:
+                # Get the two signals
+                s1={'ds':dsx_currentxplane, 'z':zref, 'y':yref+y}
+                s2={**s1, 'y':yref+y+sep_y}
+
+                mycoh, mypsd = calc_coherence_2signals(s1,s2, strname='lat', interval=interval, window_length=window_length, window=window, normal='xNormal')
+                mycoh = mycoh.expand_dims('y').assign_coords({'y':[y]})
+                coh_y.append(mycoh)
+
+            coh_y = xr.concat(coh_y, dim='y')
+            coh_ysep.append(coh_y.expand_dims('sep_y').assign_coords({'sep_y':[sep_y]}))
+        coh_ysep = xr.concat(coh_ysep, dim='sep_y')
+        coh_ysep_xnormal.append(coh_ysep.expand_dims('xplane').assign_coords({'xplane':[xplane]}))
+    coh_ysep_xnormal = xr.concat(coh_ysep_xnormal, dim='xplane')
+
+    # Get rid of the single xplane level for single xplanes (TurbSim data, for instance)
+    coh_ysep_xnormal = coh_ysep_xnormal.squeeze(drop=True)
+    
+    if outputPath is not None:
+        coh_ysep_xnormal.to_zarr(os.path.join(outputPath,f'coh_ysep_xnormal_{window_length}.zarr'))
+
+    return coh_ysep_xnormal
+
+
 
 
