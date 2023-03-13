7c6b79bd27a01aa5f954eafae2839391566e4d30
Regis Thedin <registhedin@gmail.com>
2023-03-13 17:07:33 -0600

    Add parallel processing of AMR-Wind boxes for FAST.Farm

---
+++ postprocess_amr_boxes2vtk.py
@@ -0,0 +1,204 @@
+#!/home/rthedin/.conda/envs/ssrs_env_scratch/bin/python
+
+#SBATCH --job-name=pp_vtk
+#SBATCH --output amr2post_vtk.log.%j
+#SBATCH --nodes=1
+#SBATCH --time=2-00
+#SBATCH --account=car
+#SBATCH --mem=160G
+# #SBATCH --qos=high
+
+# ----------------------------------------------------------------------- #
+# postprocess_boxes2vtk.py                                                #
+#                                                                         #
+# Post process AMR-Wind output boxes for FAST.Farm and save vtk files for #
+# each group (each individual sampling box). This script should be called #
+# once for each output netcdf file and the group is an optional argument. #
+# If group is not specified, all groups will be saved to vtk. If you need #
+# the boxes faster, consider submitting this script once per group of the #
+# high-resolution box. The output vtk is saved in $case/processedData.    #
+#                                                                         #
+# Usage:                                                                  #
+# postprocess_boxes2vtk.py -p <fullpath> -f <ncfile> [-g <group>]         #
+#                                                                         #
+# Example usage:                                                          #
+#    cd /full/path/to/amrwind/case                                        #
+#    path=$(pwd -P)                                                       #
+#    cd post_processing                                                   #
+#    ls  # see what sampling box files are available.                     #
+#    sbatch postprocess_boxes2vtk.py [-J <jobname>] -p $path              #
+#                                          -f lowres40000.nc              #
+#    sbatch postprocess_boxes2vtk.py [-J <jobname>] -p $path              #
+#                    -f highres40000.nc -g HighT1_inflow0deg              #
+#                                                                         #
+# Regis Thedin                                                            #
+# Mar 13, 2023                                                            #
+# regis.thedin@nrel.gov                                                   #
+# ----------------------------------------------------------------------- #
+
+import argparse
+import numpy as np
+import os, time
+from itertools import repeat
+from multiprocessing import Pool, freeze_support
+from windtools.amrwind.post_processing  import Sampling
+
+def main(samplingboxfile, pppath, requestedgroup, outpath, dt, itime, ftime, steptime, offsetz):
+
+    # -------- CONFIGURE RUN
+    samplingboxpath = os.path.join(pppath,samplingboxfile)
+    s = Sampling(samplingboxpath)
+
+    groups = s.groups
+
+    if requestedgroup is None:
+        if len(groups) > 1:
+            raise ValueError(f'The ncfile {samplingboxfile} contains more than one '\
+                             f'group: {groups}. Specify -g <groupstring> for single group.')
+        else:
+            group = group[0]
+            print(f'No group requested. Saving the only one present, {group}.')
+    elif requestedgroup in groups:
+        group = requestedgroup
+        print(f'Saving VTK related to requested group {group}')
+    else:
+        raise ValueError(f'Requested group {requestedgroup} is not valid. '\
+                         f'Value groups: {groups}')
+
+    s.getGroupProperties(group=group)
+    outpathgroup = os.path.join(outpath, group)
+    if not os.path.exists(outpathgroup):
+        os.makedirs(outpathgroup)
+
+    print(f'Group {group} has {s.ndt} saved time steps.')
+    if itime==0 and ftime==-1:
+        print(f'  Saving all of them.')
+    else:
+        print(f'  Saving the time steps between {itime} and {ftime}, out of {s.ndt}.')
+
+    if ftime > s.ndt:
+        print(f'Final time step requested {ftime} is larger than what is available in'\
+              f'the array, {s.ndt}. Saving up to {s.ndt} instead.')
+        ftime = s.ndt
+    if ftime == -1:
+        ftime = s.ndt
+
+    # Split all the time steps in arrays of roughly the same size
+    chunks =  np.array_split(range(itime,ftime), 36)
+    # Now, get the beginning and end of each separate chunk
+    itime_list = [i[0]    for i in chunks]
+    ftime_list = [i[-1]+1 for i in chunks] 
+    print(f'itime_list is {itime_list}')
+    print(f'ftime_list is {ftime_list}')
+
+    p = Pool()
+    ds_ = p.starmap(s.to_vtk, zip(repeat(group),         # group
+                                  repeat(outpathgroup),  # outputPath
+                                  repeat(True),          # verbose
+                                  repeat(offsetz),       # offset in z
+                                  itime_list,            # itime
+                                  ftime_list,            # ftime
+                                 )
+                              )
+    print('Finished.')
+    
+
+if __name__ == '__main__':
+
+    # ------------------------------------------------------------------------------
+    # -------------------------------- PARSING INPUTS ------------------------------
+    # ------------------------------------------------------------------------------
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--path", "-p",   type=str, default=os.getcwd(),
+                        help="case full path (default cwd)")
+    parser.add_argument("--ncfile", "-f", type=str, default=None,
+                        help="netcdf sampling planes")
+    parser.add_argument("--dt", "-dt",    type=float, default=None,
+                        help="time step for adding datetime to xarray (optional)")
+    parser.add_argument("--group", "-g",  type=str, default=None,
+                        help="group within netcdf file to be read, if more than one is available")
+    parser.add_argument("--itime", "-itime",  type=int, default=0,
+                        help="sampling time step to start saving the data")
+    parser.add_argument("--ftime", "-ftime",  type=int, default=-1,
+                        help="sampling time step to end saving the data")
+    parser.add_argument("--steptime", "-step",  type=int, default=1,
+                        help="sampling time step increment to save the data")
+    parser.add_argument("--offsetz", "-offsetz",  type=float, default=None,
+                        help="Offset in the x direction, ensuring a point at hub height")
+
+    args = parser.parse_args()
+
+    # Parse inputs
+    path     = args.path
+    ncfile   = args.ncfile
+    dt       = args.dt
+    group    = args.group
+    itime    = args.itime
+    ftime    = args.ftime
+    steptime = args.steptime
+    offsetz  = args.offsetz
+
+    # ------------------------------------------------------------------------------
+    # --------------------------- DONE PARSING INPUTS ------------------------------
+    # ------------------------------------------------------------------------------
+
+    if path == '.':
+        path = os.getcwd()
+
+    # We assume the case path was given, but maybe it was $case/post* or $case/proc*
+    # Let's check for that and fix it
+    if os.path.basename(path) == 'post_processing' or os.path.basename(path) == 'processedData':
+        path = os.path.split(path)[0]
+
+    pppath = os.path.join(path,'post_processing')
+    outpath = os.path.join(path,'processedData')
+
+    # ------- PERFORM CHECKS
+    if not os.path.exists(path):
+        parser.error(f"Path {path} does not exist.")
+
+    if not os.path.exists(pppath):
+        raise ValueError (f'Path {pppath} does not exist.')
+
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+
+    if isinstance(ncfile,str):
+        if not ncfile.endswith('.nc'):
+            raise ValueError (f"Received single string as ncfiles, but it does not end with .nc.")
+        if not os.path.isfile(os.path.join(pppath,ncfile)):
+            raise ValueError(f"File {ncfile} does not exist.")
+    else:
+        raise ValueError(f"the ncfile should be a string. Received {ncfile}.")
+
+    if dt is not None and  not isinstance(dt,(float,int)):
+        raise ValueError(f'dt should be a scalar.')
+
+    if steptime < 1:
+        raise ValueError(f'The time step increment should be >= 1.')
+
+    if itime < 0:
+        raise ValueError(f'The initial time step should be >= 0.')
+
+    if ftime != -1:
+        if ftime < itime:
+            raise ValueError(f'The final time step should be larger than the'\
+                             f'initial. Received itime={itime} and ftime={ftime}.')
+
+    if offsetz is None:
+        print(f'!!! WARNING: no offset in z has been given. Ensure you will have a point at hub height.')
+        offsetz=0
+
+    # ------------------------------------------------------------------------------
+    # ---------------------------- DONE WITH CHECKS --------------------------------
+    # ------------------------------------------------------------------------------
+
+    print(f'Reading {path}/{ncfile}')
+
+    print(f'Starting job at {time.ctime()}')
+    freeze_support()
+    main(ncfile, pppath, group, outpath, dt, itime, ftime, steptime, offsetz)
+    print(f'Ending job at   {time.ctime()}')
+
